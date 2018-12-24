d66d30cc2bdfe780a6aa756c7a322803efd8b303
i-am-sk <saikrishna106@yahoo.com>
2018-12-24 20:04:17 +0530

    go vscode

--- pointers/01/main.go
+++ pointers/01/main.go
@@ -6,22 +6,23 @@ import (
 
 func main()  {
 
-	// var a = 42
+	var a = 42
 
-	// fmt.Println(a)
+	fmt.Println(a)
 
-	// var b *int
-	// *b = &a
+	var b = &a
 	
-	// fmt.Printf("%d\n",&a)	 //assigning b gives the address of "a" where it is stored
+	fmt.Printf("%d\n",&a)	 //assigning b gives the address of "a" where it is stored
 
-	// fmt.Println(b)   //assigning b gives the address of "a" where it is stored
-	// fmt.Println(*b) //assigning *b gives the value of "a" where it is stored
+	fmt.Println(b)   //assigning b gives the address of "a" where it is stored
+	fmt.Println(*b) //assigning *b gives the value of "a" where it is stored
 
-	// *b = 56
+	*b = 56
 
-	// fmt.Println(a)
+	fmt.Println(a)
 
+	//======================================>
+	fmt.Println("===============================>")
 	i, j := 42, 2701
 
 	p := &i         // point to i
